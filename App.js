a2bcaa22e3f3f9afbe6a1c9cac00688f52bbea89
Chen <chenyu.zhu86@gmail.com>
2018-12-01 23:40:23 -0500

    initialized firebase

--- App.js
+++ App.js
@@ -8,6 +8,17 @@ import firebase from 'firebase'
 
 export default class App extends React.Component {
 
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyBPLaAGZBkpwiwJRxBqDdMNbLMsTKujlRQ",
+      authDomain: "auth-1ec3c.firebaseapp.com",
+      databaseURL: "https://auth-1ec3c.firebaseio.com",
+      projectId: "auth-1ec3c",
+      storageBucket: "auth-1ec3c.appspot.com",
+      messagingSenderId: "450076585261"
+    })
+  }
+
   render() {
     return (
       <View >
